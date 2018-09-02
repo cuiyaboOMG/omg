01a2343d07dd824e168671d0067a44ca741b3c75
cyb <1946158792@qq.com>
2018-09-02 12:41:35 +0800

    添加service类

---
+++ src/main/java/com/omg/service/UserService.java
@@ -0,0 +1,9 @@
+package com.omg.service;
+
+/**
+ * @Auther: cui
+ * @Date: 2018-09-02 12:19
+ * @Description:
+ */
+public interface UserService {
+}
---
+++ src/main/java/com/omg/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.omg.service.impl;
+
+/**
+ * @Auther: cui
+ * @Date: 2018-09-02 12:20
+ * @Description:
+ */
+public class UserServiceImpl {
+}
