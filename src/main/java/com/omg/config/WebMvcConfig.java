e479b16d87db5457f9252128d511a9421442c84b
cyb <m15190317625@163.com>
2018-10-19 16:39:50 +0800

    1.登录拦截器

--- src/main/java/com/omg/config/WebMvcConfig.java
+++ src/main/java/com/omg/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 * @Author:         cyb
 * @CreateDate:     2018/10/15 9:42
 */
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     Logger logger= LoggerFactory.getLogger(this.getClass());
