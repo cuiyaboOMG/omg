510327d090111f8e2cf1ea035b4078b461094be9
cyb <1946158792@qq.com>
2018-12-15 19:55:06 +0800

    登录拦截

--- src/main/java/com/omg/config/interceptor/LoginInterceptor.java
+++ src/main/java/com/omg/config/interceptor/LoginInterceptor.java
@@ -1,34 +1,25 @@
 package com.omg.config.interceptor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.stereotype.Component;
+        import org.springframework.web.servlet.HandlerInterceptor;
+        import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
 
 /**
-* @Author:         cyb
-* @CreateDate:     2018/10/15 9:42
-*/
+ * @Author:         cyb
+ * @CreateDate:     2018/10/15 9:42
+ */
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
     Logger logger= LoggerFactory.getLogger(this.getClass());
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-/*    	String token = httpServletRequest.getHeader("token");
-        if("dev".equals(configBean.getSpringProfilesActive())){
-            // 开发环境绕过身份验证
-            return true;
-        }
-        if(!userService.checkLoginStatus(token,"").getData()){
-            httpServletResponse.setContentType("application/json;charset=UTF-8");
-            httpServletResponse.getWriter().print(JSONObject.toJSONString(new FailResult(ErrorEnum.LOGIN_TOKENINVALID)));
-            return false;
-        }*/
+        System.out.println("进入鉴权");
         return true;
     }
 
