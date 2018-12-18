3bf92f263d671a4d53d537b09838385b7cc42549
cyb <m15190317625@163.com>
2018-12-18 15:45:00 +0800

    1.通用重复性校验修改bug

--- src/main/java/com/omg/mapper/CommonMapper.java
+++ src/main/java/com/omg/mapper/CommonMapper.java
@@ -3,6 +3,7 @@ package com.omg.mapper;
 import com.omg.domain.RepeatBean;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,5 +11,5 @@ import java.util.Map;
 * @CreateDate:     2018/11/28 13:41
 */
 public interface CommonMapper {
-    Map<String,Object> getExistsCount(@Param("table")String table, @Param("params") Map<String, Object> map, @Param("majorBean")RepeatBean majorBean);
+    List<Object> getExistsCount(@Param("table")String table, @Param("params") Map<String, Object> map, @Param("majorBean")RepeatBean majorBean);
 }
