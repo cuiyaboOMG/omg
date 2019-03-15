0281b95d5c886d75ce12e56533023792ecb99522
cyb <m15190317625@163.com>
2019-03-15 17:56:33 +0800

    1.spring session 漏提交

--- src/main/java/com/omg/config/interceptor/LoginInterceptor.java
+++ src/main/java/com/omg/config/interceptor/LoginInterceptor.java
@@ -1,25 +1,31 @@
 package com.omg.config.interceptor;
 
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.stereotype.Component;
-        import org.springframework.web.servlet.HandlerInterceptor;
-        import org.springframework.web.servlet.ModelAndView;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author:         cyb
  * @CreateDate:     2018/10/15 9:42
  */
 @Component
+@Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
-    Logger logger= LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SessionRepository<?> sessionRepository;
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        System.out.println("进入鉴权");
+        //通过自定义token 查找session
+        Session session = sessionRepository.findById("");
         return true;
     }
 
