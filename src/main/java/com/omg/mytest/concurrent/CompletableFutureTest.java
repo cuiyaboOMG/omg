b2fb7fbddd6528084086b6675ec6663609072b1e
cui.yabo <cui.yabo@chinaredstar.com>
2019-11-18 14:56:23 +0800

    java8 CompletableFuture 使用

--- src/main/java/com/omg/mytest/concurrent/CompletableFutureTest.java
+++ src/main/java/com/omg/mytest/concurrent/CompletableFutureTest.java
@@ -1,14 +1,17 @@
 package com.omg.mytest.concurrent;
 
+import com.google.common.collect.Lists;
 import com.omg.entity.User;
 import com.omg.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Created by gp-0096 on 2019/1/9.
@@ -28,17 +31,61 @@ public class CompletableFutureTest {
         return null;
     }
 
-    private String test(){
+    private static String test(){
         return "etwt";
     }
 
+    private static void thenAccept() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> test_one = CompletableFuture.completedFuture(test()).thenApply(s -> s + "one");
+        String s = test_one.get();
+        System.out.println(s);
+    }
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFutureTest completableFutureTest = new CompletableFutureTest();
-        completableFutureTest.add().thenCompose(v ->{return completableFutureTest.transfer();});
-        CompletableFuture<String> stringCompletableFuture = completableFutureTest.add().thenApply(str -> str + "1111");
-        System.out.println(stringCompletableFuture.get());
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello").thenCompose(string -> CompletableFuture.supplyAsync(() -> string + " world"));
-        System.out.println(completableFuture.get());
+        long start = System.currentTimeMillis();
+        init().thenCompose(users->{
+            //进行具体的业务操作
+            List<CompletionStage<User>> stages = users.stream().map(u -> rating(u.getAge()).thenApply(r -> {
+                u.setAge(r);
+                return u;
+            })).collect(Collectors.toList());
+            CompletableFuture<Void> future = CompletableFuture.allOf(stages.toArray(new CompletableFuture[stages.size()]));
+            return future.thenApply(v->stages.stream().map(CompletionStage::toCompletableFuture).map(CompletableFuture::join).collect(Collectors.toList()));
+        }).whenComplete((users,th)->{
+            if(th == null){
+                users.forEach(System.out::println);
+            }else{
+                throw new RuntimeException(th);
+            }
+        }).toCompletableFuture().join();
+        long end = System.currentTimeMillis();
+        System.out.println("总计耗时："+(end-start)+" ms");
+    }
+
+    private static CompletionStage<List<User>> init(){
+        User user1 = new User().builder().age(11).name("张三").build();
+        User user2 = new User().builder().age(12).name("李四").build();
+        ArrayList<User> infos = Lists.newArrayList();
+        infos.add(user1);
+        infos.add(user2);
+        return CompletableFuture.supplyAsync(()-> infos);
+    }
+
+
+    static CompletionStage<Integer> rating(int manufacturer) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                simulateDelay();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+            return manufacturer+1;
+        }).exceptionally(th -> -1);
+    }
+
+    private static void simulateDelay() throws InterruptedException {
+        Thread.sleep(5000);
     }
 
 }
