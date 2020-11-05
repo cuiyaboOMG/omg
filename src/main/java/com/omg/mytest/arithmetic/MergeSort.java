dd6ce82321fed817c16bfddd84e68e2283fb7056
H.cui.yabo <cui.yabo@RedstarClouds.com>
2020-11-05 16:58:12 +0800

    归并排序v2

--- src/main/java/com/omg/mytest/arithmetic/MergeSort.java
+++ src/main/java/com/omg/mytest/arithmetic/MergeSort.java
@@ -45,9 +45,48 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int [] str = {8,2,1,3,6,5};
-        int[] sort = sort(str);
-        for (int i: sort){
+        mergeSort(str);
+        System.out.println("排序结果");
+        for (int i: str){
             System.out.println(i);
+        }
+    }
+
+
+    private static void mergeSort(int[] array){
+        int [] temp = new int[array.length];
+        mergeSortImpl(array,0,array.length-1,temp);
+    }
+
+    public static void mergeSortImpl(int array[],int start,int end,int[] temp){
+        if(start>=end){
+            return;
+        }
+        int mid = (start+end)/2;
+        mergeSortImpl(array, start,mid, temp);//一分为二
+        mergeSortImpl(array, mid+1,end, temp);
+        merge(array,start,mid,end,temp);//递归到最低层，两个两个分组排序。
+    }
+
+    public static void merge(int[] array,int start,int mid,int end,int [] temp){
+        int left = start;
+        int right = mid +1;
+        int index = start;
+        while(left<=mid && right<=end){
+            if(array[left]<array[right]){
+                temp[index++] = array[left++];
+            }else{
+                temp[index++] = array[right++];
+            }
+        }
+        while(left<=mid){
+            temp[index++] = array[left++];
+        }
+        while (right<=end){
+            temp[index++] = array[right++];
+        }
+        for(index = start;index<=end;index++){
+            array[index] = temp[index];
         }
     }
 }
